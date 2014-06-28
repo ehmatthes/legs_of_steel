d189bdd29a151adfb5202612856b618aaa187b5a
Eric Matthes <ehmatthes@gmail.com>
2014-06-28 07:01:56 -0800

    Cleaned and commented parser.

--- xml_grabber.py
+++ xml_grabber.py
@@ -8,56 +8,36 @@ from datetime import datetime
 tree = ET.parse('tracks.gpx')
 root = tree.getroot()
 
-"""
-print(root.tag)
-print(root.attrib)
-for child in root:
-    pass#print(child)
-
-print("---")
-print(root[2])
-for child in root[2]:
-    pass#print(child)
-
-for trkpt in root.findall('trkpt'):
-    print trkpt.attrib
-"""
-
-
 lats, lons, timestamps, elevations = [], [], [], []
 
-
+# Loop through all descendent elements in root tree.
 for desc in list(root.iter()):
+    # Focus on trackpoints.
     if 'trkpt' in desc.tag:
-        #print('\n---')
-        #print(desc.attrib)
+        # Store lat and lon of each trackpoint.
         lat = float(desc.attrib['lat'])
         lon = float(desc.attrib['lon'])
         lats.append(lat)
         lons.append(lon)
+        # Loop through all descendents of each trackpoint.
         for desc_2 in list(desc.iter()):
-            #print(desc_2)
+            # Store timestamp and elevation of each trackpoint.
             if 'time' in desc_2.tag:
-                #print(desc_2.text)
-                timestamp = desc_2.text
                 timestamp = datetime.strptime(desc_2.text, '%Y-%m-%dT%H:%M:%SZ')
                 timestamps.append(timestamp)
             if 'ele' in desc_2.tag:
                 elevation = float(desc_2.text)
                 elevations.append(elevation)
 
+# Assert all lists same length.
 print(len(lats), len(lons), len(timestamps), len(elevations))
 
-
-
+# Find unique days that have tracks.
 mdy_prev = (0,0,0)
 for ts in timestamps:
-    ts_prev = ts
-    #print(ts.month)
     month = datetime.strftime(ts, '%B')
     day = datetime.strftime(ts, '%d')
     year = datetime.strftime(ts, '%Y')
     if (month, day, year) != mdy_prev:
         print month, day, year
-    #print month, day, year
     mdy_prev = (month, day, year)
